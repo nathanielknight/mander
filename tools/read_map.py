3b6a68d8ad5b88af94e6739401f44b46959db06e
Nathaniel Knight <nathaniel.ep@gmail.com>
2016-11-26 11:03:10 -0800

    Add a district parser to read_map.py
    This commit adds a district parser which reads districts from a text
    map. District HQs are denoted with a capital R or B. It also adds
    assertions that the number of cells and districts on a map are odd (to
    prevent ties).
    
    For example, the district map
    
        Rbb
        brR
    
    would have two districts, one with an HQ at (0,0) and one with an HQ
    at (2,1). It would also throw errors because it has an even number of
    cells and because it has an even number of districts.

--- tools/read_map.py
+++ tools/read_map.py
@@ -4,27 +4,61 @@
 
 import sys
 
-def cell(i, j, c):
-    template = "(({}, {}), {})"
-    if c == 'r':
-        content = 'Red'
-    elif c == 'b':
-        content = 'Blue'
-    else:
-        return False
-    return template.format(i, j, content)
-
 
 def cells(rows):
-    for i, col in enumerate(rows):
-        for j, c in enumerate(col):
+
+    def cell(i, j, c):
+        template = "(({}, {}), {})"
+        if c == 'r':
+            content = 'Red'
+        elif c == 'b':
+            content = 'Blue'
+        else:
+            return False
+        return template.format(i, j, content)
+
+    for j, col in enumerate(rows):
+        for i, c in enumerate(col):
             parsed = cell(i, j, c)
             if parsed:
                 yield parsed
 
 
+def districts(source):
+    '''A list of districts from a text map
+
+    Takes a text map with district HQs reqpresented as capital Rs and
+    Bs, . Ensures that there are an odd number of districts.
+    '''
+    rows = source.splitlines()
+
+    def district(id_no, hq_coord):
+        (i, j) = hq_coord
+        format = "{{ id = {} , hq = ({}, {}) , assigned = Set.emtpy }}"
+        return format.format(id_no, i, j)
+
+    def hq_coords():
+        for j, col in enumerate(rows):
+            for i, c in enumerate(col):
+                if c.isupper():
+                    yield (i,j)
+
+    districts = [district(id_no, coord) for id_no, coord
+                 in enumerate(sorted(hq_coords()))]
+    district_count = len(districts)
+    assert_msg = "An odd number of districts is required ({} given)"
+    assert district_count % 2 != 0, assert_msg.format(district_count)
+    return districts
+
+
 def demograph(source):
     rows = source.lower().splitlines()
+    demo_cells = list(cells(rows))
+
+    assert_msg = "An add number of cells is required ({} given)"
+    cell_count = len(demo_cells)
+    assert cell_count % 2 != 0, assert_msg.format(cell_count)
+
     template = "[ {} ]"
     content = ", ".join(cells(rows))
     return template.format(content)
