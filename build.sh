91f51599b54ee19910bdc72e0d5bf76b71fd0fb0
Nathaniel Knight <nathaniel.ep@gmail.com>
2016-11-26 14:23:49 -0800

    Fail fast in the build script
    The build script's output isn't always easy to parse if something fails,
    so this commit has it exit with code 1 if any of the steps fail.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -r output
-elm-make elm-src/Main.elm --output output/app.js
-lessc less-src/style.less output/style.css
-cp static-src/* output/
+elm-make elm-src/Main.elm --output output/app.js || exit 1
+lessc less-src/style.less output/style.css || exit 1
+cp static-src/* output/ || exit 1
