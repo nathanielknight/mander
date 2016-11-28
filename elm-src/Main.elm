405871f62323324a5cd6a0c654aa3677d6aab45d
Nathaniel Knight <nathaniel.ep@gmail.com>
2016-11-27 20:17:52 -0800

    Update map reading script to generate Elm data
    This commit updates the map reading script to generate an array of
    Bureaugraphs suitable for importing into Elm. It's now suitable for
    building levels to include in the game.

--- elm-src/Main.elm
+++ elm-src/Main.elm
@@ -7,52 +7,22 @@ import Set
 import Html.App
 
 import Data exposing(..)
+import Game
 import Update
 import View
 
 ---------------------------------------------------------------------
 
-exampleBureaugraph0 = { id = 0
-                      , fullSize = 3
-                      , demograph =
-                          (Dict.fromList
-                               [ ((0,0), Red) , ((1,0), Red) , ((2,0), Blue)
-                               , ((0,1), Blue), ((1,1), Blue), ((2,1), Blue)
-                               , ((0,2), Blue), ((1,2), Red), ((2,2), Red)
-                               ])
-                      , districts =
-                          (Dict.fromList
-                               [ (1, {id = 1, hq = (0,0), assigned = Set.empty})
-                               , (2, {id = 2, hq = (1,1), assigned = Set.empty})
-                               , (3, {id = 3, hq = (2,2), assigned = Set.empty})
-                               ])
-                      }
-exampleBureaugraph1 = { id = 0
-                      , fullSize = 5
-                      , demograph =
-                          (Dict.fromList
-                               [ ((1,0), Red), ((2,0), Red), ((3,0), Blue), ((4,0), Red)
-                               , ((1,1), Red), ((2,1), Blue), ((3,1), Blue), ((4,1), Blue), ((5,1), Blue)
-                               , ((0,2), Blue), ((1,2), Red), ((2,2), Red), ((3,2), Blue), ((4,2), Blue)
-                               , ((1,3), Blue)])
-                      , districts =
-                          (Dict.fromList
-                               [ (1, {id = 1, hq = (3,0), assigned = Set.empty})
-                               , (2, {id = 2, hq = (4,1), assigned = Set.empty})
-                               , (3, {id = 3, hq = (2,2), assigned = Set.empty})
-                               ])
-                      }
-
-exampleModel = { activeBureaugraphId = 0
-               , maxAvailableBureaugraphId = 0
-               , bureaugraphs = Array.fromList [exampleBureaugraph0, exampleBureaugraph1]
-               , activeDistrictId = 0
-               , drawing = False
-               }
+initModel = { activeBureaugraphId = 0
+            , maxAvailableBureaugraphId = 0
+            , bureaugraphs = Game.bureaugraphs
+            , activeDistrictId = 0
+            , drawing = False
+            }
 
 main =
     Html.App.beginnerProgram
-        { model = exampleModel
+        { model = initModel
         , view = View.view
         , update = Update.update
         }
--- tools/read_map.py
+++ tools/read_map.py
@@ -1,41 +1,53 @@
 #!/usr/bin/env python
-'''
+'''Reads maps from text files
 '''
 
 import sys
 
+def split_rows(source):
+    rows = source.splitlines()
+    rows = filter(lambda x: bool(x) and not x.isspace(),
+                  rows)
+    return rows
 
-def cells(rows):
 
-    def cell(i, j, c):
+def demograph(source):
+    rows = split_rows(source)
+    def iter_cells():
+        for j, col in enumerate(rows):
+            for i, c in enumerate(col):
+                if not c.isspace():
+                    yield (i, j, c)
+
+    cells = list(iter_cells())
+
+    assert_msg = "An odd number of cells is required ({} given)"
+    cell_count = len(cells)
+    assert cell_count % 2 != 0, assert_msg.format(cell_count)
+
+    return cells
+
+
+def render_demograph(cells):
+
+    def cell_kv(i, j, c):
         template = "(({}, {}), {})"
-        if c == 'r':
+        if c.lower() == 'r':
             content = 'Red'
-        elif c == 'b':
+        elif c.lower() == 'b':
             content = 'Blue'
         else:
-            return False
+            msg = "Can't handle character {} at ({}, {})"
+            raise ValueError(msg.format(c, i, j))
         return template.format(i, j, content)
 
-    for j, col in enumerate(rows):
-        for i, c in enumerate(col):
-            parsed = cell(i, j, c)
-            if parsed:
-                yield parsed
+    template = "(Dict.fromList [ {} ])"
+    return template.format(", ".join(cell_kv(*c) for c in cells))
+
 
 
 def districts(source):
-    '''A list of districts from a text map
-
-    Takes a text map with district HQs reqpresented as capital Rs and
-    Bs, . Ensures that there are an odd number of districts.
-    '''
-    rows = source.splitlines()
-
-    def district(id_no, hq_coord):
-        (i, j) = hq_coord
-        format = "{{ id = {} , hq = ({}, {}) , assigned = Set.emtpy }}"
-        return format.format(id_no, i, j)
+    rows = split_rows(source)
 
     def hq_coords():
         for j, col in enumerate(rows):
@@ -43,7 +55,7 @@ def districts(source):
                 if c.isupper():
                     yield (i,j)
 
-    districts = [district(id_no, coord) for id_no, coord
+    districts = [(id_no+1, coord) for id_no, coord
                  in enumerate(sorted(hq_coords()))]
     district_count = len(districts)
     assert_msg = "An odd number of districts is required ({} given)"
@@ -51,20 +63,62 @@ def districts(source):
     return districts
 
 
-def demograph(source):
-    rows = source.lower().splitlines()
-    demo_cells = list(cells(rows))
+def render_districts(districts):
 
-    assert_msg = "An add number of cells is required ({} given)"
-    cell_count = len(demo_cells)
-    assert cell_count % 2 != 0, assert_msg.format(cell_count)
+    def district_kv(id_no, hq):
+        assert id_no != 0, "Districts can't have id=0 (that's the empty district)"
+        template = "{{ id = {} , hq = ({}, {}) , assigned = Set.empty }}"
+        distr = template.format(id_no, *hq)
+        return "({}, {})".format(id_no, distr)
 
-    template = "[ {} ]"
-    content = ", ".join(cells(rows))
-    return template.format(content)
+    template = "(Dict.fromList [ {} ])"
+    district_source = "\n , ".join(list(district_kv(id_no, hq) for id_no, hq in districts))
+    return template.format(district_source)
+
+
+def bureaugraph(id_no, source):
+    try:
+        dgraph = demograph(source)
+        distrs = districts(source)
+    except Exception as e:
+        print("Exception during bureaugraph {}".format(id_no))
+        print(source)
+        raise e
+
+    even_msg = "Uneven cells per district ({} cells, {} districts in bgraph {})"
+    assert len(dgraph) % len(distrs) == 0, even_msg.format(
+        len(dgraph), len(distrs), id_no)
+    fullsize = int(len(dgraph) / len(distrs))
+
+    dgraph_source = render_demograph(dgraph)
+    dist_source = render_districts(distrs)
+
+    template = "{{ id = {}, fullSize = {}, demograph = {}, districts = {} }}"
+
+    return template.format(id_no, fullsize, dgraph_source, dist_source)
+
+
+def game(source):
+    game_template = '''module Game exposing(bureaugraphs)
+
+-- Generated file. Do not edit directly.
+
+import Array
+import Dict
+import Set
+
+import Data exposing(..)
+
+bureaugraphs = (Array.fromList [ {} ])'''
+    sources = source.split("\n\n")
+    bgraphs = [bureaugraph(i, s) for i,s in enumerate(sources) if s]
+
+    return game_template.format("\n , ".join(bgraphs))
+
+
 
 
 if __name__ == "__main__":
-    rows = sys.stdin.read()
-    output = demograph(rows)
+    source = sys.stdin.read()
+    output = game(source)
     sys.stdout.write(output)
